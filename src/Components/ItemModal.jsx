48133dc9a4018c9c22251533b0583007d0abe8b6
Ion Stici <ionsticidev@gmail.com>
2024-07-15 16:02:34 +0300

    Improvements, fix order total, add license

---
+++ src/Components/ItemModal.jsx
@@ -0,0 +1,25 @@
+export default function ItemModal({ dessert }) {
+  const { image, name, cart, price } = dessert;
+
+  return (
+    <div className="mb-4 flex items-center border-b border-rose_100 pb-4 text-sm font-semibold last:mb-6 last:pb-6">
+      <div className="mr-4 h-12 w-12 shrink-0">
+        <img
+          className="size-full rounded-[4px]"
+          src={image?.thumbnail}
+          alt={name}
+        />
+      </div>
+
+      <div className="mr-4 min-w-0">
+        <p className="mb-2 truncate">{name}</p>
+        <p className="space-x-2">
+          <span className="text-red">{cart}x</span>
+          <span className="font-normal text-rose_500">@${price}</span>
+        </p>
+      </div>
+
+      <p className="ml-auto text-base">${cart * price}</p>
+    </div>
+  );
+}
--- src/Components/OrderModal.jsx
+++ src/Components/OrderModal.jsx
@@ -3,6 +3,7 @@ import { useDesserts } from "../contexts/DessertsContext";
 import { createPortal } from "react-dom";
 import { ReactSVG } from "react-svg";
 import Button from "../ui/Button";
+import ItemModal from "./ItemModal";
 
 export default function OrderModal({ setCartIsOpen }) {
   const { getDessertsCart, clearCart } = useDesserts({ setCartIsOpen });
@@ -12,6 +13,10 @@ export default function OrderModal({ setCartIsOpen }) {
     document.documentElement.classList.add("no-scroll");
     return () => document.documentElement.classList.remove("no-scroll");
   }, []);
+
+  const totalPrice = desserts.reduce((acc, { cart, price }) => {
+    return acc + cart * price;
+  }, 0);
 
   const handleCloseModal = ({ target }) => {
     if (target.dataset.type !== "outside") return;
@@ -27,7 +32,7 @@ export default function OrderModal({ setCartIsOpen }) {
     <div
       onClick={handleCloseModal}
       data-type="outside"
-      className="hide-scrollbar fixed inset-0 h-dvh place-content-center overflow-scroll bg-rose_900/50 py-[95px] backdrop-blur-sm"
+      className="hide-scrollbar fixed inset-0 h-dvh place-content-center overflow-scroll bg-rose_900/50 py-[95px] backdrop-blur-sm md:p-[95px]"
     >
       <div className="mx-auto max-w-[450px] rounded-[12px] bg-white px-6 pb-6 pt-10 md:max-w-[688px] md:p-10 lg:max-w-[592px]">
         <ReactSVG className="pb-6" src="images/icon-order-confirmed.svg" />
@@ -41,39 +46,14 @@ export default function OrderModal({ setCartIsOpen }) {
 
         <div className="mb-8 rounded-lg bg-rose_50 p-6">
           <div>
-            {desserts.map(({ image, name, cart, price }, i) => {
-              return (
-                <div
-                  className="mb-4 flex items-center border-b border-rose_100 pb-4 text-sm font-semibold last:mb-6 last:pb-6"
-                  key={i}
-                >
-                  <div className="mr-4 h-12 w-12 shrink-0">
-                    <img
-                      className="size-full rounded-[4px]"
-                      src={image?.thumbnail}
-                      alt=""
-                    />
-                  </div>
-
-                  <div className="mr-4 min-w-0">
-                    <p className="mb-2 truncate">{name}</p>
-                    <p className="space-x-2">
-                      <span className="text-red">{cart}x</span>
-                      <span className="font-normal text-rose_500">
-                        @${price}
-                      </span>
-                    </p>
-                  </div>
-
-                  <p className="ml-auto text-base">${cart * price}</p>
-                </div>
-              );
+            {desserts.map((dessert, i) => {
+              return <ItemModal key={i} dessert={dessert} />;
             })}
           </div>
 
           <p className="flex items-center justify-between">
             <span className="text-sm">Order Total</span>
-            <span className="text-2xl font-bold">$46.50</span>
+            <span className="text-2xl font-bold">${totalPrice}</span>
           </p>
         </div>
 
